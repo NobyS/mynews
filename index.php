ea2f25a7cd41d515077793a5c56ae1ea2591aca2
Nawelko24 <norbertschmoll@gmail.com>
2019-11-27 22:50:06 +0100

    Favicon

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <title>Aktuelle Nachrichten ansehen!</title>
     <link rel="stylesheet" href="vendor/twitter/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 </head>
 <div class="container">
         <div class="panel panel-primary">
@@ -41,8 +43,12 @@ echo "<br>";
 
 $api = "7c496bf13fa54708aba065e72e3ee6c9";
 $country = "country=de";
-$pagesize= "pagesize=10";
+$pagesize= "pagesize=20";
+
+
 $ch = curl_init("https://newsapi.org/v2/top-headlines?$country&apiKey=$api&category=$cat&$pagesize");
+
+
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 $result = json_decode($result);
