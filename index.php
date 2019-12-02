cd63647b2573ea3bff3232c37febad1ea012c34e
Nawelko24 <norbertschmoll@gmail.com>
2019-12-02 12:34:50 +0100

    Suche

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ $pagesize= "pagesize=20";
 if (strlen($cat1)>0)
 {
     $s = $cat1;
-    $ch = curl_init('https://newsapi.org/v2/everything?q='."$s".'&apiKey=$api&$pagesize&language=de');
+    $ch = curl_init('https://newsapi.org/v2/everything?q='.$s.'&apiKey=$api&$pagesize&language=de');
 }else
 {
     $ch = curl_init("https://newsapi.org/v2/top-headlines?$country&apiKey=$api&category=$cat&$pagesize");
