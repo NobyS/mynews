31bc6c09b06014a1128cc9834a0663baadf4f4c0
Nawelko24 <norbertschmoll@gmail.com>
2020-04-25 13:51:50 +0200

    Neu

--- login.php
+++ login.php
@@ -6,6 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
     <title>Login</title>
+    <style>
+    form-group {
+
+        padding: 10px;
+    }
+
+
+    </style>
+
+
 </head>
 <body>
 
@@ -34,7 +44,7 @@
         
             
         
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Anfragen</button>
             <a href="logout.php"></a>
         </form>
 </div>
